366e3ae9d93a76d702996626881ac1626449109d
g3n35i5 <janfschmidt@me.com>
2019-02-10 16:22:42 +0100

    Disable Buy Button and Delete button when the cart is empty

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -56,6 +56,10 @@ export class ShopComponent implements OnInit, OnDestroy {
     this.cartSubscription.unsubscribe();
   }
 
+  disableBuyButton(): boolean {
+    return this.disableInput || this.shopService.cartIsEmpty();
+  }
+
   submitCart(): void {
     this.shopService.submitCart();
   }
