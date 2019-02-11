ee174edb663c15f61c69e3645f4aa65bab4a5b1f
g3n35i5 <janfschmidt@me.com>
2019-02-11 07:58:46 +0100

    Adding snackbar service

---
+++ src/app/services/snackbar/snackbar.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+  constructor(
+    private snackBar: MatSnackBar
+  ) {
+  }
+
+  /** Open a snackbar with the given data. */
+  openSnackBar(message: string) {
+    this.snackBar.open(message, '', {
+      duration: 6000
+    });
+  }
+}
